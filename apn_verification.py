55d5010ff1af10ea7d80a1a0962c342ddfa851b0
amichne <austinmichne@Austins-MacBook-Pro.local>
2017-11-26 03:33:12 -0700

    Point based, multiple db APN, sped up

--- apn_verification.py
+++ apn_verification.py
@@ -1,4 +1,5 @@
 from shapely.geometry import shape, asShape
+import shapely
 import pymysql
 import getpass
 import json
@@ -13,31 +14,38 @@ class apn_verification(object):
         # host, user, password, database, table_name, drop
         maz_file = open(filepath['maz'], 'r')
         maz_set = json.load(maz_file)
-        # maz_set = gp.read_file(filepath['maz'])
-        # maz_file.close()
+        maz_file.close()
         print("Maz Loaded")
+
         parcel_file = open(filepath['parcel'], 'r')
         parcel_set = json.load(parcel_file)
-        # parcel_set = gp.read_file(filepath['parcel'])
-        # parcel_file.close()
+        parcel_file.close()
         print("Parcel Loaded")
-        conn = pymysql.connect(host=database['host'], user=database['user'], password=database['password'], database=database['database'])
+        try:
+            print("System Level DB created")
+            conn = pymysql.connect(host=database['host'], user=database['user'], password=database['password'], database=database['database'])
+        except KeyError as keyErr:
+            print("File Level DB created")
+            conn = sql.connect(database['database'])
         cur = conn.cursor()
         if (database['drop'] == True):
             cur.execute(("DROP TABLE if exists {}").format(database['table_name']))
-            exec_str = ("""CREATE table {0} (apn int unsigned, maz int unsigned)""").format(database['table_name'])
+            exec_str = ("CREATE table {0} (apn CHAR(9), maz INT UNSIGNED NOT NULL)").format(database['table_name'])
             cur.execute(exec_str)
-        else:
-            if (database['drop'] == True):
-                cur.execute(("DROP TABLE {0};").format(database['table_name']))
-                exec_str = ("CREATE table {0} (apn INT UNSIGNED NOT NULL PRIMARY KEY, maz INT UNSIGNED NOT NULL, usage TINYINT UNSIGNED NOT NULL)").format(database['table_name'])
-                cur.execute(exec_str)
+        # else:
+        #     if (database['drop'] == True):
+        #         cur.execute(("DROP TABLE {0};").format(database['table_name']))
+        #         exec_str = ("CREATE table {0} (apn INT UNSIGNED NOT NULL PRIMARY KEY, maz INT UNSIGNED NOT NULL, usage TINYINT UNSIGNED NOT NULL)").format(database['table_name'])
+        #         cur.execute(exec_str)
         insert_tuple = list()
         progress = 0
-        total = len(maz_set['features'])
-        for bounding in maz_set['features']:
-            bounding_shape = shape(bounding['geometry'])
-            if (progress >= (.025*total)):print(".25")
+        total = len(parcel_set['features'])
+        for feature in parcel_set['features']:
+            try:
+                feature_shape = (shape(feature['geometry'])).representative_point()
+            except ValueError as topExcep:
+                feature_shape = shape(feature['geometry'])
+            if (progress >= (.025*total)):print(".25");conn.commit()
             if (progress >= (.1*total)):print("1");conn.commit()
             if (progress >= (.2*total)):print("2");conn.commit()
             if (progress >= (.3*total)):print("3");conn.commit()
@@ -48,14 +56,16 @@ class apn_verification(object):
             if (progress >= (.8*total)):print("8");conn.commit()
             if (progress >= (.9*total)):print("9");conn.commit()
             progress += 1
-            for feature in parcel_set['features']:
-                feature_shape = shape(feature['geometry'])
+            for bounding in maz_set['features']:
+                bounding_shape = shape(bounding['geometry'])
                 if feature_shape.within(bounding_shape):
-                    insert_tuple.append(tuple([feature['properties']['APN'], bounding['properties']['MAZ_ID_10'], 0]))
-                    parcel_set['features'].remove(feature)
+                    insert_tuple = tuple([feature['properties']['APN'], bounding['properties']['MAZ_ID_10']])
+                    print(insert_tuple)
+                    exec_str = ("INSERT into {0} values {1};").format(database['table_name'], insert_tuple)
+                    cur.execute(exec_str)
+                    conn.commit()
+                    # parcel_set['features'].remove(feature)
                     break
-        exec_str = ("INSERT into {0} (apn, maz) values ({1}, {1});").format(database['table_name'], insert_tuple)
-        cur.executemany(exec_str)
         conn.commit()
         conn.close()
 
@@ -63,6 +73,6 @@ if __name__ == "__main__":
     x = apn_verification()
     files = {'parcel': 'Parcels_All/all_parcel.geojson', 'maz':'real_maz/maz.geojson'}
     pw = getpass.getpass()
-    db_param = {'host':'localhost', 'user':'root', 'password':pw, 'database':'apn', 'table_name':'test', 'drop':True}
+    db_param = {'user':'root', 'password':pw, 'database':'apn', 'table_name':'apn_test', 'drop':True, 'host':'localhost'}
     x.parsing_apns(files, db_param)
 
