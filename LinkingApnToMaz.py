28d312c8b64241b365284abf975b87b33c3d3b1e
amichne <austinmichne@gmail.com>
2018-03-30 11:59:44 -0700

    .

--- LinkingApnToMaz.py
+++ LinkingApnToMaz.py
@@ -78,7 +78,10 @@ class LinkingApnToMaz:
             bounding_for_maz = transform(proj_to_map, bounding_from_inp)
 
         else:
-            default_bounding = polygon.Polygon([(649054, 896498), (649192, 888749), (665535, 896129), (663998, 889026)])
+            default_bounding = polygon.Polygon([(291681.866638, 2147002.203025),
+                                                (1114836.32474, 2099088.372318),
+                                                (1092055.717785, 913579.224235),
+                                                (341912.702254, 946252.321431)])
             proj_to_map = partial(pyproj.transform, pyproj.Proj(init='epsg:2223'), pyproj.Proj(init='epsg:{}'.format(self.crs)))
             bounding_for_maz = transform(proj_to_map, default_bounding)
         self.bounding_for_maz = bounding_for_maz
@@ -97,10 +100,15 @@ class LinkingApnToMaz:
     def assign_maz_per_apn(self, write_to_database=False):
         # "Meat" of the module, connection MAZ, APN, and osm_id
         # This creates the output to be used in agent plan generation
+        insert_list = list()
         if self.bounded_eval:
             maz_set_local = self.bounded_maz_set
         else:
             maz_set_local = self.maz_set
+
+        bounding_set_local = list()
+        for bounding in maz_set_local['features']:
+            bounding_set_local.append(tuple([shape(bounding['geometry']), bounding['properties']['MAZ_ID_10']]))
 
         for feature in self.parcel_set['features']:
             temp_shape = shape(feature['geometry'])
@@ -112,22 +120,20 @@ class LinkingApnToMaz:
                 except TypeError:
                     pass
             if temp_point.within(self.bounding_for_maz) or (self.bounded_eval is False):
-                for bounding in maz_set_local['features']:
-                    bounding_shape = shape(bounding['geometry'])
-                    if temp_point.within(bounding_shape):
+                for bounding_shape in bounding_set_local:
+                    if temp_point.within(bounding_shape[0]):
                         insert_tuple = tuple([feature['geometry']['coordinates'][0],
                                             feature['geometry']['coordinates'][1],
                                             feature['properties']['APN'],
-                                            bounding['properties']['MAZ_ID_10']])
-                        if write_to_database is True:
-                            exec_str = ("INSERT INTO {} values {};").format(self.table_name, insert_tuple)
-                            self.cur.execute(exec_str)
+                                            bounding_shape[1]])
+                        insert_list.append(insert_tuple)
                         if bounding['properties']['MAZ_ID_10'] in self.apn_maz:
-                            self.apn_maz[bounding['properties']['MAZ_ID_10']].append(bounding)
+                            self.apn_maz[bounding_shape[1]].append(bounding)
                         else:
-                            self.apn_maz[bounding['properties']['MAZ_ID_10']] = [list(insert_tuple)]
+                            self.apn_maz[bounding_shape[1]] = [list(insert_tuple)]
 
         if write_to_database is True:
+            self.cur.executemany(f"INSERT INTO {self.table_name} values (%s,%s,%s,%s)", tuple(insert_list))
             self.conn.commit()
             self.conn.close()
 
