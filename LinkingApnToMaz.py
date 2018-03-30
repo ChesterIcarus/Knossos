02adcf32e204efc22fe072c85e5eb31dd12a5780
amichne <austinmichne@gmail.com>
2018-03-30 12:04:24 -0700

    .

--- LinkingApnToMaz.py
+++ LinkingApnToMaz.py
@@ -154,6 +154,6 @@ if __name__ == "__main__":
         (1114836.32474, 2099088.372318),
         (1092055.717785, 913579.224235),
         (341912.702254, 946252.321431)]
-    example.set_bounding(full_ariz)
+    example.set_bounding()
     example.find_maz_in_bounds()
     example.assign_maz_per_apn(True)
